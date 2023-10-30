9684d890bd1e9b264e87efdf6b3afbf407880ced
ioanacretu2002 <ioanacretu2002@yahoo.com>
2023-10-30 23:48:12 +0200

    laborator

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,11 +1,13 @@
-﻿namespace Cretu_Ioana_Lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cretu_Ioana_Lab2.Models
 {
     public class Author
     {
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string AuthorName { get { return FirstName + " " + LastName; } }
+        public string AuthorName { get { return FirstName + " " + LastName; } } //aici e fullname in document
         public ICollection<Book>? Books { get; set; } //navigation property
 
     }
